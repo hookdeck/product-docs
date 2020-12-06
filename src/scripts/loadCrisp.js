d512c57b1e9d71cfe12c94220937b2f75e9b769a
Alexandre Bouchard <alexbouchardd@gmail.com>
2020-12-06 14:12:55 -0800

    fix: Fix crispt script

--- src/scripts/loadCrisp.js
+++ src/scripts/loadCrisp.js
@@ -3,10 +3,9 @@ if (ExecutionEnvironment.canUseDOM) {
   window.$crisp = [];
   window.CRISP_WEBSITE_ID = "a5e808e8-1191-4809-ac65-adcf72135cef";
   (function () {
-    d = document;
-    s = d.createElement("script");
+    var s = document.createElement("script");
     s.src = "https://client.crisp.chat/l.js";
     s.async = 1;
-    d.getElementsByTagName("head")[0].appendChild(s);
+    document.getElementsByTagName("head")[0].appendChild(s);
   })();
 }
