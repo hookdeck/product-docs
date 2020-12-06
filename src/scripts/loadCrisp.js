8d6cd938ce739b9700f123efa243366fa0a26247
Alexandre Bouchard <alexbouchardd@gmail.com>
2020-12-06 14:10:41 -0800

    fix: Fix tracking scripts build

--- src/scripts/loadCrisp.js
+++ src/scripts/loadCrisp.js
@@ -1,9 +1,12 @@
-window.$crisp = [];
-window.CRISP_WEBSITE_ID = "a5e808e8-1191-4809-ac65-adcf72135cef";
-(function () {
-  d = document;
-  s = d.createElement("script");
-  s.src = "https://client.crisp.chat/l.js";
-  s.async = 1;
-  d.getElementsByTagName("head")[0].appendChild(s);
-})();
+import ExecutionEnvironment from "@docusaurus/ExecutionEnvironment";
+if (ExecutionEnvironment.canUseDOM) {
+  window.$crisp = [];
+  window.CRISP_WEBSITE_ID = "a5e808e8-1191-4809-ac65-adcf72135cef";
+  (function () {
+    d = document;
+    s = d.createElement("script");
+    s.src = "https://client.crisp.chat/l.js";
+    s.async = 1;
+    d.getElementsByTagName("head")[0].appendChild(s);
+  })();
+}
