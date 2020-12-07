f33ef34773fe08df4c2db5cfde75456480be30a0
Alexandre Bouchard <alexbouchardd@gmail.com>
2020-12-06 16:30:59 -0800

    fix: Add image border

--- sidebars.js
+++ sidebars.js
@@ -5,6 +5,6 @@ module.exports = {
     Events: ['doc8', 'doc9', 'doc10'],
     Workspace: ['doc11', 'doc12', 'doc13'], 
     'Best Practices': ['doc14', 'doc15', 'doc16'],
-    'Tips and Tricks': ['doc17', 'doc18', 'doc19', 'doc20', 'doc21', 'doc22','styleguide']
+    'Tips and Tricks': ['doc17', 'doc18', 'doc19', 'doc20', 'doc21', 'doc22']
   },
 };
